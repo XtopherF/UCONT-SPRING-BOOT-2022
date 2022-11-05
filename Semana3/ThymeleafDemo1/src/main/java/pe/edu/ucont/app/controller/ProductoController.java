460e689693809c52d5df0ec75d39f93cbc4b348e
Gustavo Corone <gcoronelc@gmail.com>
2022-11-05 17:06:35 -0500

    Semana 3

--- Semana3/ThymeleafDemo1/src/main/java/pe/edu/ucont/app/controller/ProductoController.java
+++ Semana3/ThymeleafDemo1/src/main/java/pe/edu/ucont/app/controller/ProductoController.java
@@ -3,9 +3,12 @@ package pe.edu.ucont.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pe.edu.ucont.app.dto.ProductoDto;
 import pe.edu.ucont.app.service.ProductoService;
 
 @Controller
@@ -19,6 +22,22 @@ public class ProductoController {
 	public String listado(Model model) {
 		model.addAttribute("lista", productoService.obtenerProductos());
 		return "listado";
+	}
+	
+	@GetMapping("/consulta")
+	public String consulta() {
+		return "consulta";
+	}
+	
+	@PostMapping("/consulta")
+	public String consulta(String nombre, Model model) {
+		ProductoDto dto = productoService.buscar(nombre);
+		if(dto==null) {
+			model.addAttribute("mensaje", "Producto no existe");
+		} else {
+			model.addAttribute("dto", dto);
+		}
+		return "consulta";
 	}
 
 }
--- Semana3/ThymeleafDemo1/src/main/java/pe/edu/ucont/app/service/ProductoService.java
+++ Semana3/ThymeleafDemo1/src/main/java/pe/edu/ucont/app/service/ProductoService.java
@@ -13,6 +13,17 @@ public class ProductoService {
 	public List<ProductoDto> obtenerProductos(){
 		return Data.lista;
 	}
+
+	public ProductoDto buscar(String nombre) {
+		ProductoDto dto = null;
+		for(ProductoDto bean: Data.lista) {
+			if(bean.getNombre().toUpperCase().equals(nombre.toUpperCase())) {
+				dto = bean;
+				break;
+			}
+		}
+		return dto;
+	}
 	
 	
 }
